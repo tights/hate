073a7507f0da06d5ca2d56692e004e2b40fcd2ad
matt <33669870+constitute@users.noreply.github.com>
2019-06-29 03:35:07 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,7 +3,7 @@ const { prefix } = require('../config.json');
 module.exports = (client, message) => {
 
     if (message.isMentioned(client.user)) {
-        return console.log(`[MENTIONS] ${message.guild.name} > ${message.content.slice()}`)
+        return console.log(`[MENTIONS] ${message.guild.name} > ${message.author.tag} > ${message.content}`)
     } else
     if (message.channel.type === `dm`) {
         if (message.content.startsWith(prefix)) {
